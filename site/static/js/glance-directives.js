c75de7e62692a595131d2b59fffff04f459e1bfd
Jared De La Cruz <jared@labs.asti-usa.com>
2015-08-24 18:09:34 -0700

    pr username

--- site/static/js/glance-directives.js
+++ site/static/js/glance-directives.js
@@ -18,9 +18,9 @@ glanceDirectives.directive('pullRequest', ['$compile', '$filter', function($comp
       var buildHtml = function buildHtml() {
         var html = '<div class="col-md-12">'
                   + '<div class="glance-box-inner">'
-                    + '<div class="">'
-                      + '<div>Repo: ' + scope.pr.repo_name + '<span class="pull-right">' + dateFilter(scope.pr.created_at) + '</span></div>'
-                      + '<div>Title: ' + scope.pr.title + '<span class="pull-right">' + scope.pr.state + '</span></div>'
+                    + '<div class="glance-pr-container">'
+                      + '<div class="glance-pr-main"><span class="pull-right glance-pr-created_at">' + dateFilter(scope.pr.created_at) + '</span> <span class="glance-pr-repo_name"> Repo: ' + scope.pr.repo_name + '</span></div>'
+                      + '<div class="glance-pr-sub"><span class="pull-right glance-pr-user">' + scope.pr.user + '</span> <span class="glance-pr-title"> Title: ' + scope.pr.title + '</span></div>'
                     + '</div>'
                   + '</div>'
                 + '</div>';
